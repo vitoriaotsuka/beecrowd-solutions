c637a6d49ae6363ae11c2612507222ecc868070f
Vitória Otsuka <vitoriatampelli@gmail.com>
2026-02-04 10:52:20 -0300

    Resolve beecrowd 1004 - Produto Simples

---
+++ iniciante/1004-produto-simples/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int A = int.Parse(Console.ReadLine());
+        int B = int .Parse(Console.ReadLine());
+
+        int PROD = A * B;
+
+        Console.WriteLine("PROD = " + PROD);
+    }
+}
