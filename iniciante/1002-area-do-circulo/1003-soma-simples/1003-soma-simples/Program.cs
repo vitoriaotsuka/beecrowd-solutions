621facddf1c18a6f13d295b4ce5ae2a8af24e8aa
Vitória Otsuka <vitoriatampelli@gmail.com>
2026-02-04 10:16:03 -0300

    Resolve exercício 1003 - Soma Simples

---
+++ iniciante/1002-area-do-circulo/1003-soma-simples/1003-soma-simples/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int A = int.Parse(Console.ReadLine());
+        int B = int.Parse(Console.ReadLine());
+
+        int SOMA = A + B;
+
+        Console.WriteLine("SOMA = " + SOMA);
+    }
+}
