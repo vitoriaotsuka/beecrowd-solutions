1e13f568b74eaade0ec65df6c09ad5717d4dc288
Vitória Otsuka <vitoriatampelli@gmail.com>
2026-02-09 09:34:45 -0300

    Resolve beecrowd 1009 - Salário com Bônus

---
+++ iniciante/1009-salario-com-bonus/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+
+namespace salario_bonus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string vendendor = Console.ReadLine();
+            double salarioFixo = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double totVendas = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double taxa = 0.15;
+
+            double bonus = totVendas * taxa;
+
+            double salarioFinal = salarioFixo + bonus;
+
+            Console.WriteLine("TOTAL = R$ " + salarioFinal.ToString("F2", CultureInfo.InvariantCulture));
+
+
+
+
+        }
+    }
+}
