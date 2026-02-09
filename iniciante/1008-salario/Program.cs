080a1e3d6585d71cc9a2b6d6d20c3aec2b3a0abd
Vitória Otsuka <vitoriatampelli@gmail.com>
2026-02-09 09:21:10 -0300

    Resolve beecrowd 1008 - Salário

---
+++ iniciante/1008-salario/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace salario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numFuncionarios = int.Parse(Console.ReadLine());
+            int numHorasTrabalhadas = int.Parse(Console.ReadLine());
+            double valorHora = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double salario = numHorasTrabalhadas * valorHora;
+
+            Console.WriteLine("NUMBER = " + numFuncionarios);
+            Console.WriteLine("SALARY = U$ " + salario.ToString("F2", CultureInfo.InvariantCulture));
+
+
+        }
+    }
+}
