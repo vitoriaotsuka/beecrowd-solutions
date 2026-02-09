b1ae39b0cba2626c5c5e3cc47bce6b1955feb01e
Vitória Otsuka <vitoriatampelli@gmail.com>
2026-02-09 08:45:44 -0300

    Resolve beecrowd 1005 - Média 1

---
+++ 1005-media-1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel;
+using System.Globalization;
+
+
+namespace media_1
+{
+    class Programm
+    {
+        static void Main(string[] args)
+        {
+            double A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double soma = (A * 3.5) + (B * 7.5);
+            double media = soma / 11;
+
+            Console.WriteLine("MEDIA = " + media.ToString("F5"), CultureInfo.InvariantCulture);
+
+
+
+        }
+    }
+
+}
