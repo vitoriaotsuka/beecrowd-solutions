70712acbc4c0fd0926435ad453b77102294965d8
Vitória Otsuka <vitoriatampelli@gmail.com>
2026-02-09 09:05:20 -0300

    Resolve beecrowd 1006 - Média 2

---
+++ iniciante/1006-media-2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+
+namespace media_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double media = (A * 2 + B * 3 + C * 5) / 10;
+
+            Console.WriteLine("MEDIA = " + media.ToString("F1", CultureInfo.InvariantCulture));
+        }
+    }
+}
