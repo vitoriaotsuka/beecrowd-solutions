2b4ff8807fe2ac98c538772fd81cb38fc754ced3
Vitória Otsuka <vitoriatampelli@gmail.com>
2026-02-10 15:58:09 -0300

    Resolve beecrowd 1016 - Distância

---
+++ iniciante/1016-distancia/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Distancia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int distacia = int.Parse(Console.ReadLine());
+
+            int kmMinutos = distacia * 2;
+
+            Console.WriteLine($"{kmMinutos} minutos");
+        }
+        
+    }
+}
