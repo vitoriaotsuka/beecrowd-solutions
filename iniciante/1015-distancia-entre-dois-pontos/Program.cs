e49857beb4acd38246b65a8aa32cf6970d7235ce
Vitória Otsuka <vitoriatampelli@gmail.com>
2026-02-10 15:47:33 -0300

    Resolve beecrowd 1015 - Distância Entre Dois Pontos

---
+++ iniciante/1015-distancia-entre-dois-pontos/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+
+namespace Distancia {
+    class Program{
+        static void Main(string[] args){
+            string[] coordenadas1 = Console.ReadLine().Split(' ');
+            string[] coordenadas2 = Console.ReadLine().Split(' ');
+
+            double x1 = double.Parse(coordenadas1[0], CultureInfo.InvariantCulture);
+            double y1 = double.Parse(coordenadas1[1], CultureInfo.InvariantCulture);
+            double x2 = double.Parse(coordenadas2[0], CultureInfo.InvariantCulture);
+            double y2 = double.Parse(coordenadas2[1], CultureInfo.InvariantCulture);
+
+            double distancia = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+
+            Console.WriteLine(distancia.ToString("F4", CultureInfo.InvariantCulture));
+        }
+}
+}
