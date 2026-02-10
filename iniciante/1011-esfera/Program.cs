b6f7895822b7e9eca35c6e592d92c520ed731b8c
Vitória Otsuka <vitoriatampelli@gmail.com>
2026-02-10 15:06:09 -0300

    Resolve beecrowd 1011 - Esfera

---
+++ iniciante/1011-esfera/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Globalization;
+
+namespace Esfera
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double pi = 3.14159;
+            double volume = (4 / 3.0) * pi * Math.Pow(raio, 3);
+
+            Console.WriteLine("VOLUME = " + volume.ToString("F3", CultureInfo.InvariantCulture));
+
+
+        }
+    }
+}
