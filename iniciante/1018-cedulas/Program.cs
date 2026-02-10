b84205992b0bca5ed3930cc1f018836732574825
Vitória Otsuka <vitoriatampelli@gmail.com>
2026-02-10 16:10:23 -0300

    Resolve beecrowd 1018 - Cédulas

---
+++ iniciante/1018-cedulas/Program.cs
@@ -0,0 +1,24 @@
+﻿
+namespace Cedulas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+
+            if (N > 0 || N < 1000000)
+            {
+                int[] cedulas = { 100, 50, 20, 10, 5, 2, 1 };
+                Console.WriteLine(N);
+                for (int i = 0; i < cedulas.Length; i++)
+                {
+                    int quantidade = N / cedulas[i];
+                    Console.WriteLine($"{quantidade} nota(s) de R$ {cedulas[i]},00");
+                    N = N % cedulas[i];
+                }
+            }
+        }
+
+    }
+}
