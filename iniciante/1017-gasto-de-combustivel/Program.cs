96e5670b7437cd276f164f8b4eda6464a7b58229
Vitória Otsuka <vitoriatampelli@gmail.com>
2026-02-10 16:04:15 -0300

    Resolve beecrowd 1017 - Gasto de Combustível

---
+++ iniciante/1017-gasto-de-combustivel/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+
+namespace Combustivel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int horas = int.Parse(Console.ReadLine());
+            int velocidade = int.Parse(Console.ReadLine()); 
+
+            double distancia = horas * velocidade;
+            double litros = distancia / 12;
+
+            Console.WriteLine(litros.ToString("F3", CultureInfo.InvariantCulture));
+        }
+    }
+}
