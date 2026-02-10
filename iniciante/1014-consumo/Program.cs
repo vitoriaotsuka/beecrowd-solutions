b2b359d18f9ad950388bf7d4c6c81e8cec65409e
Vitória Otsuka <vitoriatampelli@gmail.com>
2026-02-10 15:38:44 -0300

    Resolve beecrowd 1014 - Consumo

---
+++ iniciante/1014-consumo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+
+namespace Consumo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int X = int.Parse(Console.ReadLine());
+            double Y = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double consumo = X / Y;
+
+            Console.WriteLine(consumo.ToString("F3", CultureInfo.InvariantCulture) + " km/l");
+
+
+        }
+    }
+}
