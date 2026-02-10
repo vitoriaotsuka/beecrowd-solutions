ef9f6f1c59b0b1230bc57688796cf4720f5cdaee
Vitória Otsuka <vitoriatampelli@gmail.com>
2026-02-10 16:16:30 -0300

    Resolve beecrowd 1019 - Conversão de Tempo

---
+++ iniciante/1019-conversao-de-tempo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Conversao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            
+            int horas = N / 3600;
+            int minutos = (N % 3600) / 60;
+            int segundos = N % 60;
+
+            Console.WriteLine($"{horas}:{minutos}:{segundos}");
+
+        }
+    }
+}
