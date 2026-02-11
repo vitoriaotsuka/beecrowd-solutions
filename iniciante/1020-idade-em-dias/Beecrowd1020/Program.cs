a6e16a4138c50b7cdc4eb066afb17459f8da3978
Vitória Otsuka <vitoriatampelli@gmail.com>
2026-02-11 15:01:11 -0300

    Resolve beecrowd 1020

---
+++ iniciante/1020-idade-em-dias/Beecrowd1020/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * ==================================================================================================================
+ *   Beecrowd 1020 - Idade em Dias
+ *   Descrição: Leia um valor inteiro correspondente à idade de uma pessoa em dias e informe-a em anos, meses e dias.
+ *   
+ *   Saída: Imprima a saída conforme exemplo fornecido.
+ * ==================================================================================================================
+*/
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int dias = int.Parse(Console.ReadLine());
+
+        int anos = dias / 365;
+        int meses = (dias % 365) / 30;
+        int diasRestantes = (dias % 365) % 30;
+
+        Console.WriteLine($"{anos} ano(s)");
+        Console.WriteLine($"{meses} mes(es)");
+        Console.WriteLine($"{diasRestantes} dia(s)");
+
+    }
+}
